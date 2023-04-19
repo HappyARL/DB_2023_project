5e39e5d3e264edbb2f5023b39ff7110d1d7d2d07
Arlan Aliyev (Арлан Алиев) <104730663+HappyARL@users.noreply.github.com>
2023-04-20 04:03:23 +0600

    Update kzrustelecom_ddl.sql

--- scripts/kzrustelecom_ddl.sql
+++ scripts/kzrustelecom_ddl.sql
@@ -1,13 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS cmp;
 
-CREATE TABLE IF NOT EXISTS cmp.client (
-  	ФИО VARCHAR(200) NOT NULL,
-  	Номер_заказа INTEGER NOT NULL,
-  	Телефон VARCHAR(11) NOT NULL,
-  	Номер_паспорта INTEGER NOT NULL PRIMARY KEY,
-  	Адрес VARCHAR(200) NOT NULL,
+-- Создание таблиц
 
-  	CONSTRAINT unique_passport_number UNIQUE (Номер_паспорта)
+CREATE TABLE IF NOT EXISTS cmp.client (
+    ФИО VARCHAR(200) NOT NULL,
+    Телефон VARCHAR(11) NOT NULL,
+    Номер_паспорта INTEGER NOT NULL UNIQUE,
+    Адрес VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmp.employee (
@@ -15,72 +14,70 @@ CREATE TABLE IF NOT EXISTS cmp.employee (
 	ФИО VARCHAR(200) NOT NULL,
 	Телефон VARCHAR(11) NOT NULL,
 	Отдел VARCHAR(200) NOT NULL,
-	Зарплата INTEGER NOT NULL,
-
-	CONSTRAINT unique_id_number UNIQUE (id_number)
+	Зарплата INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmp.order (
-	Заказчик VARCHAR(200) NOT NULL,
+	Заказчик INTEGER NOT NULL,
 	Исполнитель INTEGER NOT NULL,
 	Дата_заказа DATE NOT NULL,
-	Цена INTEGER NOT NULL,
+	Цена DECIMAL(10, 2) NOT NULL,
 	Адрес VARCHAR(200) NOT NULL,
 	Дата_исполнения DATE NOT NULL,
 	Код_услуги INTEGER NOT NULL,
-	Номер_заказа INTEGER NOT NULL PRIMARY KEY
+	Номер_заказа INTEGER NOT NULL,
 
-	CONSTRAINT клиент_заказчик FOREIGN KEY (Заказчик) REFERENCES cmp.client(Номер_паспорта),
-	CONSTRAINT сотрудник_исполнитель FOREIGN KEY (Исполнитель) REFERENCES cmp.employee(id_number),
-	CONSTRAINT адрес_заказчика FOREIGN KEY (Адрес) REFERENCES cmp.client(Номер_паспорта),
-	CONSTRAINT id_заказа FOREIGN KEY (Номер_заказа) REFERENCES cmp.client(Номер_заказа),
-	--код услуги
+	PRIMARY KEY (Заказчик, Исполнитель, Адрес, Код_услуги),
+	FOREIGN KEY (Заказчик) REFERENCES cmp.client(Номер_паспорта),
+	FOREIGN KEY (Исполнитель) REFERENCES cmp.employee (id_number),
+	FOREIGN KEY (Адрес) REFERENCES cmp.client (Адрес),
+	FOREIGN KEY (Код_услуги) REFERENCES cmp.internet (Код_услуги_интернет)
+	--FOREIGN KEY (Код_услуги) REFERENCES cmp.tv (Код_услуги_телевизор),
+	--FOREIGN KEY (Код_услуги) REFERENCES cmp.phone (Код_услуги_телефон)
 );
 
--- функция которая принимает коды услуг и создает общий код для всех
 
 CREATE TABLE IF NOT EXISTS cmp.internet (
 	Тариф VARCHAR(200) NOT NULL,
 	Скорость INTEGER NOT NULL,
 	Интернет_провайдер VARCHAR(200) NOT NULL,
-	Цена INTEGER NOT NULL,
-	Код_услуги_интернет INTEGER NOT NULL PRIMARY KEY,
-	
-	CONSTRAINT код_инет UNIQUE (Код_услуги_интернет)
+	Цена DECIMAL(10, 2) NOT NULL,
+	Код_услуги_интернет INTEGER NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS cmp.tv (
 	Тариф VARCHAR(200) NOT NULL,
 	Количество_каналов INTEGER NOT NULL,
-	Цена INTEGER NOT NULL,
-	Код_услуги_телевизор INTEGER NOT NULL PRIMARY KEY,
-	
-	CONSTRAINT код_телевизор UNIQUE (Код_услуги_телевизор)
+	Цена DECIMAL(10, 2) NOT NULL,
+	Код_услуги_телевизор INTEGER NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS cmp.phone (
-	Тариф VARCHAR(200) NOT NULL,
-	Меж_город VARCHAR(200) NOT NULL,
-	Меж_страны VARCHAR(200) NOT NULL,
-	Цена INTEGER NOT NULL,
 	Код_услуги_телефон INTEGER NOT NULL PRIMARY KEY,
-	
-	CONSTRAINT код_телефон UNIQUE (Код_услуги_телефон),
-	CONSTRAINT платный_город FOREIGN KEY (Меж_город) REFERENCES cmp.incountry(Город),
-	CONSTRAINT платная_страна FOREIGN KEY (Меж_страны) REFERENCES cmp.outcountry(Страны)
+	Цена DECIMAL(10, 2) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmp.incountry (
-	Цена INTEGER NOT NULL,
 	Город VARCHAR(200) NOT NULL,
-	
-	CONSTRAINT платные_города UNIQUE (Город)
+	Цена INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmp.outcountry (
-	Цена INTEGER NOT NULL,
-	Страны VARCHAR(200) NOT NULL,
-	
-	CONSTRAINT платная_страна UNIQUE (Страны)
+	Страна VARCHAR(200) NOT NULL,
+	Цена INTEGER NOT NULL
 );
 
+-- Связывание таблиц и преобразование 
+SELECT CONCAT_WS('_', ic.Город, oc.Страны) AS Выбор_клиента,
+       p.Код_услуги_телефон
+FROM cmp.phone p
+JOIN cmp.incountry ic ON p.Меж_город = ic.Город
+JOIN cmp.outcountry oc ON p.Меж_страны = oc.Страны;
+
+-- Обратное преобразование кода в выбор клиента
+SELECT CONCAT_WS(', ', SUBSTRING(p.Код_услуги_телефон, 1, CHARINDEX('_', p.Код_услуги_телефон)-1),
+                         ic.Город,
+                         SUBSTRING(p.Код_услуги_телефон, CHARINDEX('_', p.Код_услуги_телефон)+1)) AS Выбор_клиента
+FROM cmp.phone p
+JOIN cmp.incountry ic ON p.Меж_город = ic.Город
+JOIN cmp.outcountry oc ON p.Меж_страны = oc.Страны;
