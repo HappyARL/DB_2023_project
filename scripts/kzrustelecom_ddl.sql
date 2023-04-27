2334fb5d73b16678c17b7c8a154aeedabc767f63
Arlan Aliyev (Арлан Алиев) <104730663+HappyARL@users.noreply.github.com>
2023-04-27 18:55:43 +0600

    Update kzrustelecom_ddl.sql

--- scripts/kzrustelecom_ddl.sql
+++ scripts/kzrustelecom_ddl.sql
@@ -1,3 +1,5 @@
+-- Создание схемы
+
 CREATE SCHEMA IF NOT EXISTS cmp;
 
 -- Создание таблиц
@@ -17,67 +19,58 @@ CREATE TABLE IF NOT EXISTS cmp.employee (
 	Зарплата INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS cmp.order (
-	Заказчик INTEGER NOT NULL,
-	Исполнитель INTEGER NOT NULL,
-	Дата_заказа DATE NOT NULL,
-	Цена DECIMAL(10, 2) NOT NULL,
-	Адрес VARCHAR(200) NOT NULL,
-	Дата_исполнения DATE NOT NULL,
-	Код_услуги INTEGER NOT NULL,
-	Номер_заказа INTEGER NOT NULL,
-
-	PRIMARY KEY (Заказчик, Исполнитель, Адрес, Код_услуги),
-	FOREIGN KEY (Заказчик) REFERENCES cmp.client(Номер_паспорта),
-	FOREIGN KEY (Исполнитель) REFERENCES cmp.employee (id_number),
-	FOREIGN KEY (Адрес) REFERENCES cmp.client (Адрес),
-	FOREIGN KEY (Код_услуги) REFERENCES cmp.internet (Код_услуги_интернет)
-	--FOREIGN KEY (Код_услуги) REFERENCES cmp.tv (Код_услуги_телевизор),
-	--FOREIGN KEY (Код_услуги) REFERENCES cmp.phone (Код_услуги_телефон)
+CREATE TABLE IF NOT EXISTS cmp.services (
+    Код_услуги VARCHAR(200) NOT NULL PRIMARY KEY,
+    Цена DECIMAL(10, 2) NOT NULL,
+    Название VARCHAR(200) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS cmp.order (
+    Заказчик INTEGER NOT NULL,
+    Исполнитель INTEGER NOT NULL,
+    Дата_заказа DATE NOT NULL,
+    Цена DECIMAL(10, 2) NOT NULL,
+    Адрес VARCHAR(200) NOT NULL,
+    Дата_исполнения DATE NOT NULL,
+    Код_услуги VARCHAR(200) NOT NULL,
+    Номер_заказа INTEGER NOT NULL,
+
+    PRIMARY KEY (Заказчик, Исполнитель, Адрес, Код_услуги),
+    FOREIGN KEY (Заказчик) REFERENCES cmp.client(Номер_паспорта),
+    FOREIGN KEY (Исполнитель) REFERENCES cmp.employee (id_number),
+    FOREIGN KEY (Адрес) REFERENCES cmp.client (Адрес),
+    FOREIGN KEY (Код_услуги) REFERENCES cmp.services (Код_услуги)
+);
 
 CREATE TABLE IF NOT EXISTS cmp.internet (
 	Тариф VARCHAR(200) NOT NULL,
 	Скорость INTEGER NOT NULL,
 	Интернет_провайдер VARCHAR(200) NOT NULL,
 	Цена DECIMAL(10, 2) NOT NULL,
-	Код_услуги_интернет INTEGER NOT NULL PRIMARY KEY
+	Код_услуги_интернет VARCHAR(200) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS cmp.tv (
 	Тариф VARCHAR(200) NOT NULL,
 	Количество_каналов INTEGER NOT NULL,
 	Цена DECIMAL(10, 2) NOT NULL,
-	Код_услуги_телевизор INTEGER NOT NULL PRIMARY KEY
+	Код_услуги_телевизор VARCHAR(200) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS cmp.phone (
-	Код_услуги_телефон INTEGER NOT NULL PRIMARY KEY,
+	Тариф VARCHAR(200) NOT NULL,
+	Код_услуги_телефон VARCHAR(200) NOT NULL PRIMARY KEY,
 	Цена DECIMAL(10, 2) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmp.incountry (
+    Код VARCHAR(200) NOT NULL PRIMARY KEY,
 	Город VARCHAR(200) NOT NULL,
 	Цена INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmp.outcountry (
+    Код VARCHAR(200) NOT NULL PRIMARY KEY,
 	Страна VARCHAR(200) NOT NULL,
 	Цена INTEGER NOT NULL
 );
-
--- Связывание таблиц и преобразование 
-SELECT CONCAT_WS('_', ic.Город, oc.Страны) AS Выбор_клиента,
-       p.Код_услуги_телефон
-FROM cmp.phone p
-JOIN cmp.incountry ic ON p.Меж_город = ic.Город
-JOIN cmp.outcountry oc ON p.Меж_страны = oc.Страны;
-
--- Обратное преобразование кода в выбор клиента
-SELECT CONCAT_WS(', ', SUBSTRING(p.Код_услуги_телефон, 1, CHARINDEX('_', p.Код_услуги_телефон)-1),
-                         ic.Город,
-                         SUBSTRING(p.Код_услуги_телефон, CHARINDEX('_', p.Код_услуги_телефон)+1)) AS Выбор_клиента
-FROM cmp.phone p
-JOIN cmp.incountry ic ON p.Меж_город = ic.Город
-JOIN cmp.outcountry oc ON p.Меж_страны = oc.Страны;
